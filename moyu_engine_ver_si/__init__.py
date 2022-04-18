96f62a3a6c462e118d3602a264932bfd5af10b04
WilsonVinson <cprewilsonvinson@gmail.com>
2022-04-18 14:55:24 +0800

    main

---
+++ moyu_engine_ver_si/__init__.py
@@ -0,0 +1,11 @@
+
+print('''
+ = MoYu Engine SpaceInvaders Version = 
+Welcome to join MoYu Studio!
+
+Powered by:
+MoYu Studio
+Hello to visit our Github Community : https://github.com/MoYuStudio ''')
+
+from .config import *
+from .module import *
