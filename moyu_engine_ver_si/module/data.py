46bb9162abe758ad201ba5c0b7f4035f232ecb85
WilsonVinson <cprewilsonvinson@gmail.com>
2022-04-19 13:01:06 +0800

    main

--- moyu_engine_ver_si/module/data.py
+++ moyu_engine_ver_si/module/data.py
@@ -2,13 +2,32 @@
 import sqlite3
 
 class Data:
+    '''
+=== MoYu Engine === 
+[ Data 数据模块 ]
+数据库模块，用于读取数据库中的数据，并将数据存储在内存中。
+变量：
+    数据库路径 database_path = 'string'
+    数据库列表 database_list = {'string': ['string','string'],...}
+    数据库格式 database_format = {'string': ['string','string'],...}
+    配置 config = {}
+函数：
+    加载数据库 load()
+    更新数据库并提交 update()
+    '''
     def __init__(self):
+        # 数据库路径
         self.database_path = 'moyu_engine_ver_si/config/'
+        # 数据库列表
         self.database_list = {'config': ['main','window']}
+        # 数据库格式
         self.database_format = {'config': ['config','data']}
+        # 配置
         self.config = {}
 
+    # 加载数据库
     def load(self):
+        # 历遍数据库列表
         for database in self.database_list:
             database_connect = sqlite3.connect(self.database_path+database+'.db')
             db = database_connect.cursor()
@@ -22,7 +41,9 @@ class Data:
         
         return self.config
 
+    # 更新数据库并提交
     def update(self):
+        # 历遍数据库列表
         for database in self.database_list:
             database_connect = sqlite3.connect(self.database_path+database+'.db')
             db = database_connect.cursor()
@@ -35,6 +56,7 @@ class Data:
             database_connect.close()
 
 if __name__ == '__main__':
+    print(Data.__doc__)
     data = Data()
     data.load()
     C = data.config['config']
