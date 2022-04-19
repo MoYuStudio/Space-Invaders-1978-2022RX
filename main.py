d27cb945a96380cce7da50ae675d3d3da8eabf7a
WilsonVinson <cprewilsonvinson@gmail.com>
2022-04-19 12:50:36 +0800

    main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
 import moyu_engine_ver_si as ME
-C = ME.config
 
 data = ME.Data
 data.load('window')
--- moyu_engine_ver_si/module/data.py
+++ moyu_engine_ver_si/module/data.py
@@ -1,27 +1,46 @@
 
 import sqlite3
 
-from moyu_engine_ver_si.config import config as C
-
 class Data:
     def __init__(self):
-        self.database = sqlite3.connect('moyu_engine_ver_si/config/config.db')
-        self.db = self.database.cursor()
-        pass
+        self.database_path = 'moyu_engine_ver_si/config/'
+        self.database_list = {'config': ['main','window']}
+        self.database_format = {'config': ['config','data']}
+        self.config = {}
 
-    def load(self,database_tablename):
-        C.config[str(database_tablename)] = {}
-        for data in self.db.execute('select * from {}'.format(database_tablename)):
-            C.config[str(database_tablename)][data[0]] = data[1]
-        self.database.commit()
-        self.database.close()
+    def load(self):
+        for database in self.database_list:
+            database_connect = sqlite3.connect(self.database_path+database+'.db')
+            db = database_connect.cursor()
+            self.config[database] = {}
+            for table in self.database_list[database]:
+                self.config[database][table] = {}
+                for data in db.execute('SELECT * FROM ' + table):
+                    self.config[database][table][data[0]] = data[1]
+            database_connect.commit()
+            database_connect.close()
+        
+        return self.config
 
-    def updata(self):
-        for table in C.config:
-            for data in C.config[table]:
-                self.db.execute('update {} set info = "{}" where name = "{}"'.format(table,C.config[table][data],data))
-        self.database.commit()
-        self.database.close()
+    def update(self):
+        for database in self.database_list:
+            database_connect = sqlite3.connect(self.database_path+database+'.db')
+            db = database_connect.cursor()
+            if database == 'config':
+                for table in self.config[database]:
+                    for data in self.config[database][table]:
+                        execute = 'update ' + table + ' set ' + self.database_format[database][1] + ' = ? where ' + self.database_format[database][0] + ' = ?'
+                        db.execute(execute, (str(self.config[database][table][data]), data))
+            database_connect.commit()
+            database_connect.close()
 
 if __name__ == '__main__':
-    pass
+    data = Data()
+    data.load()
+    C = data.config['config']
+    C['main']['test'] = 1
+    data.update()
+    print(data.config)
+
+
+
